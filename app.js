6dc7640dfbe2392f080ce249c1e5bb20770e9589
Caiioo <caiomiranda946@gmail.com>
2025-08-02 06:02:00 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,16 +2,16 @@ const express = require("express");
 const session = require("express-session");
 const path = require("path");
 
-onst authRoutes = require('./routes/auth');
-const dashboardRoutes = require("./rotas/dashboard");
+const authRoutes = require('./routes/auth');
+const dashboardRoutes = require('./routes/dashboard');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("público"));
+app.use(express.static("public")); // cuidado: "público" deve ser "public" se for o nome da pasta real
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "visualizações")); // <- ESSENCIAL na Render!
+app.set("views", path.join(__dirname, "views")); // troque "visualizações" por "views" se for o nome da pasta real
 
 app.use(
   session({
