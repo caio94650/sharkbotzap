74d71f2581d77c7f6cdcb0993f93ed2819e6ee5d
Caiioo <caiomiranda946@gmail.com>
2025-08-02 06:00:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const session = require("express-session");
 const path = require("path");
 
-const authRoutes = require("./rotas/auth");
+onst authRoutes = require('./routes/auth');
 const dashboardRoutes = require("./rotas/dashboard");
 
 const app = express();
