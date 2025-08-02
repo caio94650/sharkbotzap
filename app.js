f6b8431d1940454be6076ee883d7a98f205ef1d2
Caiioo <caiomiranda946@gmail.com>
2025-08-02 05:45:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const session = require("express-session");
 const path = require("path");
 
 const authRoutes = require("./rotas/auth");
-const dashboardRoutes = require("./rotas/painel");
+const dashboardRoutes = require("./rotas/dashboard");
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("público"));
+
 app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "visualizações")); // <- ESSENCIAL na Render!
 
 app.use(
   session({
